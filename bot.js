3575c11f84a9192dca0ffe2d4b59cc5c4259d542
P1M5 <playeronemilionandfive@gmail.com>
2021-05-31 21:34:30 +0300

    Init commit

---
+++ bot.js
@@ -0,0 +1,13 @@
+const { Client } = require("discord.js");
+const config = require("./config.json");
+const client = new Client();
+
+client.on("message", function (message) {
+    if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+    if(message.content.toLowerCase() === "xa!hello") {
+        message.reply("Hi, I am a music bot in development");
+    };
+})
+
+
+client.login(config.BOT_TOKEN);
