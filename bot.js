9825d2393092f704fb39d0230e911dcd96abaff2
P1M5 <playeronemilionandfive@gmail.com>
2021-06-01 19:11:40 +0300

    Fix issues

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Client();
 
 client.on("message", function (message) {
     if(!message.content.startsWith(config.prefix) || message.author.bot) return;
-    if(message.content.toLowerCase() === "xa!hello") {
+    if(message.content.toLowerCase() === "--hello") {
         message.reply("Hi, I am a music bot in development");
     };
 })
