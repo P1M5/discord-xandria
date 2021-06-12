00b12de0f1ced3f65bdbc2272fece58903578469
P1M5 <playeronemilionandfive@gmail.com>
2021-06-12 22:36:58 +0300

    Minor fixes

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # discord-xandria
 
 Xandria is a Music bot for Discord
-
-## Config
-
-The config file has been replaced with a [template](https://github.com/P1M5/discord-xandria/blob/main/config.json) due to the sensitive information it contains, but you can replace it with your own values and it will work
--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const { Client } = require("discord.js");
-const config = require("./config.json");
+const dotenv = require("dotenv").config();
 const client = new Client();
 
-client.on("message", function (message) {
-    if(!message.content.startsWith(config.prefix) || message.author.bot) return;
-    if(message.content.toLowerCase() === "--hello") {
-        message.reply("Hi, I am a music bot in development");
-    };
-})
+client.on("message", function(message) {
+	if(!message.content.startsWith(process.env.prefix) || message.author.bot) return;
+	if(message.content.toLowerCase() === "--hello") {
+		message.reply("Hi, I am a music bot in development");
+	}
+});
 
 
-client.login(config.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
